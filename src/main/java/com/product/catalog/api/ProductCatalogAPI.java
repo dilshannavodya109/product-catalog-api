ae6ad49928fbebfc25eb4dbcaf2675b6eb1dc652
dilshannavodya109 <dilshan.navodya109@gmail.com>
2025-12-28 13:58:53 +0100

    add main class

---
+++ src/main/java/com/product/catalog/api/ProductCatalogAPI.java
@@ -0,0 +1,11 @@
+package com.product.catalog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogAPI {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogAPI.class, args);
+    }
+}
